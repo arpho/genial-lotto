db7134d976426ef28a619802c55c110c984a3a50
arpho <damicogiuseppe77@gmail.com>
2022-05-17 18:59:38 +0200

    Setting up our Firebase config
    https://blog.logrocket.com/rest-api-firebase-cloud-functions-typescript-firestore/

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,7 +1,6 @@
 import * as functions from "firebase-functions";
+import * as express from "express";
 
-
-export const helloWorld = functions.https.onRequest((request, response) => {
-  functions.logger.info("Hello logs!", {structuredData: true});
-  response.send("Hello from Firebase!");
-});
+const app = express();
+app.get("/", (req, res) => res.status(200).send("Hey there!"));
+exports.app = functions.https.onRequest(app);
