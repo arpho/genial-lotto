9b1deb56e29e9822b15ad8b329c59d66b19f7a93
arpho <damicogiuseppe77@gmail.com>
2022-05-18 19:59:42 +0200

    entry crud completto quasi
    le funzioni sono stae create mancano le rotte

--- functions/src/entryController.ts
+++ functions/src/entryController.ts
@@ -35,5 +35,73 @@ const addEntry = async (req: Request, res: Response) => {
     }
   }
 };
-export {addEntry};
 
+const updateEntry = async (req: Request, res: Response) => {
+  const {body: {text, title}, params: {entryId}} = req;
+
+  try {
+    const entry = db.collection("entries").doc(entryId);
+    const currentData = (await entry.get()).data() || {};
+    const entryObject = {
+      title: title || currentData.title,
+      text: text || currentData.text,
+    };
+
+    await entry.set(entryObject).catch((error) => {
+      res.status(400).json({
+        status: "error",
+        message: error.message,
+      });
+    });
+
+    res.status(200).json({
+      status: "success",
+      message: "entry updated successfully",
+      data: entryObject}
+    );
+  }catch (error) {
+    if (error instanceof Error) {
+      res.status(500).json(error["message"]);
+    }
+  }
+};
+
+const getAllEntries = async (req: Request, res: Response) => {
+  try {
+    const allEntries: EntryType[] = [];
+    const querySnapshot = await db.collection("entries").get();
+    querySnapshot.forEach((doc: any) => allEntries.push(doc.data()));
+    res.status(200).json(allEntries);
+  } catch (error) {
+    if (error instanceof Error) {
+     res.status(500).json(error.message);
+    }
+   }
+}
+
+const deleteEntry = async (req: Request, res: Response) => {
+  const { entryId } = req.params
+
+  try {
+    const entry = db.collection("entries").doc(entryId)
+
+    await entry.delete().catch ((error) => {
+       res.status(400).json({
+        status: "error",
+        message: error.message
+      })
+    })
+
+     res.status(200).json({
+      status: "success",
+      message: "entry deleted successfully",
+    })
+  }
+  catch (error) { 
+    if (error instanceof Error){
+     res.status(500).json(error.message);
+    }
+  }
+  };
+  export { addEntry, getAllEntries, updateEntry, deleteEntry }
+  
