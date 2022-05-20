d8a7aa07f8bbc3775ab3e7ff3066982d796cdd0f
arpho <damicogiuseppe77@gmail.com>
2022-05-20 21:20:39 +0200

    api for extraction
    implemented api to put extraction on firestore and deprecated scaper

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,11 +1,17 @@
 import * as functions from "firebase-functions";
 import * as express from "express";
-import {addEntry, deleteEntry, getAllEntries, updateEntry} from "./entryController";
+import {addEntry,
+  deleteEntry,
+  getAllEntries,
+  updateEntry,
+} from "./entryController";
+import {addExtraction} from "./extractionController";
 
 const app = express();
 app.get("/", (req, res) => res.status(200).send("Hey there!"));
 app.post("/entries", addEntry);
 exports.app = functions.https.onRequest(app);
-app.get('/entries', getAllEntries)
-app.patch('/entries/:entryId', updateEntry)
-app.delete('/entries/:entryId', deleteEntry)
+app.get("/entries", getAllEntries);
+app.patch("/entries/:entryId", updateEntry);
+app.delete("/entries/:entryId", deleteEntry);
+app.post("/extraction", addExtraction);
