fda316f9869e7706419bea236f2584252abf6bf9
arpho <damicogiuseppe77@gmail.com>
2022-05-24 19:44:51 +0200

    corrected spec file
    expect must e inside it

--- src/app/modules/helpers/makeReference.spec.ts
+++ src/app/modules/helpers/makeReference.spec.ts
@@ -2,5 +2,8 @@ import { ReferenceFactory } from "./makeReference"
 
 describe('url well formed',()=>{
     const makeReference = new ReferenceFactory()
+    it("url is ok",()=>{
+
     expect(makeReference.referenceFactory('user','test')).toEqual('user/test')
+    })
 })
