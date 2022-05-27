57a7266cf4b2365e9a7797932c18f483606dbc61
arpho <damicogiuseppe77@gmail.com>
2022-05-27 18:34:03 +0200

    navigate to login
    if the users is not logged in we redirect to login

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,6 +3,7 @@ import {db} from "./../../functions/src/configs/firebase"
 import {initializeApp} from "firebase/app"
 import { getAuth, onAuthStateChanged } from "firebase/auth";
 import { configs } from './configs/credentials';
+import { Router } from '@angular/router';
 @Component({
   selector: 'app-root',
   templateUrl: 'app.component.html',
@@ -18,14 +19,20 @@ export class AppComponent  implements OnInit{
     { title: 'Spam', url: '/folder/Spam', icon: 'warning' },
   ];
   public labels = ['Family', 'Friends', 'Notes', 'Work', 'Travel', 'Reminders'];
-  constructor() {}
+  constructor(public router:Router) {}
   ngOnInit(): void {
     const app = initializeApp(configs.firebase)
     const auth = getAuth()
     onAuthStateChanged(auth,async (user)=>{
+      if( user){
       const token = await user.getIdTokenResult(true)
-		console.log("user ok è",user)
-		console.log("token.claims",token.claims)
-    })
+		  console.log("user ok è",user)
+		  console.log("token.claims",token.claims)
+    }else{
+      this.router.navigate(["/users/login"])
+    }
+    }
+    
+    )
   }
 }
