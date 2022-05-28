cbe7e5731b86adb33ef0a111ffec383bbb66b4fb
arpho <damicogiuseppe77@gmail.com>
2022-05-28 09:23:59 +0200

    estrazioniService
    ready to get data

--- src/app/services/extractions/estrazioni.service.ts
+++ src/app/services/extractions/estrazioni.service.ts
@@ -1,9 +1,16 @@
 import { Injectable } from '@angular/core';
+import { BehaviorSubject, Observable } from 'rxjs';
+import { Extraction } from 'src/app/models/extractionModel';
 
 @Injectable({
   providedIn: 'root'
 })
 export class EstrazioniService {
+  collection= "extractions"
+
+  extractions_list:Extraction[]
+  _extractions: BehaviorSubject<Array<Extraction>> = new BehaviorSubject([])
+  readonly extractions:Observable<Array<Extraction>> = this._extractions.asObservable()
 
   constructor() { }
 }
