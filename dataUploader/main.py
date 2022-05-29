94a6dce7550488421ef4f8f652527695c18cb419
arpho <damicogiuseppe77@gmail.com>
2022-05-29 17:00:08 +0200

    json
    modified to write a json file

--- dataUploader/main.py
+++ dataUploader/main.py
@@ -3,6 +3,7 @@
 # Press Maiusc+F10 to execute it or replace it with your code.
 # Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
 from extraction import  Extraction
+import json
 import requests
 url = "https://us-central1-genial-lotto.cloudfunctions.net/app/extraction"
 
@@ -10,17 +11,16 @@ url = "https://us-central1-genial-lotto.cloudfunctions.net/app/extraction"
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
     count = 0
+    dataList= []
+    f = open("data.json",'w')
     with open("./archivio/archivio..txt") as fp:
         Lines = fp.readlines()
         for line in Lines:
             data = Extraction(line.strip().split()).setdata()
             print ("pushing", data)
-            result = requests.post(url, data)
-            print("status: ", result.status_code)
-            print(result.text)
-            if(result.ok):
-                count +=1
-                print("inserted", data)
+            dataList.append(data)
+        json_string = json.dumps(dataList)
+        f.write(json_string)
     print("inserted {} estrazioni".format(count))
     """
     line = "1234 18/02/1939 NA 26 56 68 65 39"
