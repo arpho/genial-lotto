8868ea2d8a18067a76fe3df2f0cfa53151fccf78
arpho <damicogiuseppe77@gmail.com>
2022-05-29 12:37:54 +0200

    getting data
    we get data from firestoree

--- src/app/folder/folder.page.ts
+++ src/app/folder/folder.page.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
+import { Extraction } from '../models/extractionModel';
 import { EstrazioniService } from '../services/extractions/estrazioni.service';
 
 @Component({
@@ -9,14 +10,31 @@ import { EstrazioniService } from '../services/extractions/estrazioni.service';
 })
 export class FolderPage implements OnInit {
   public folder: string;
+  showSpinner= true
+  weels:string[] = []
+  dateEstrazioni:string[]=[]
 
   constructor(private activatedRoute: ActivatedRoute,public estrazioni:EstrazioniService) { }
 
   ngOnInit() {
     this.folder = this.activatedRoute.snapshot.paramMap.get('id');
     this.estrazioni.extractions.subscribe((items)=>{
+      this.showSpinner= items.length ==0
       console.log("got items",items.length)
+      items.sort((a:Extraction,b:Extraction)=>{
+        return a._dateInmsec-b._dateInmsec// ordinamento crescente dal più vecchio al più recente
+      }).forEach((e:Extraction)=>{
+        this.weels.push(e.weel)
+        this.weels =  Array.from( new Set(this.weels))
+        this.dateEstrazioni.push(e.date)
+        this.dateEstrazioni= Array.from(new Set(this.dateEstrazioni))
+      })
+      console.log("ruote",this.weels)
+      console.log("date",this.dateEstrazioni.length)
+    
     })
+
+
   }
 
 }
