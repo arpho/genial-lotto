79799e0bcbdaec7c19701cdf579ccb4427cf3afe
arpho <damicogiuseppe77@gmail.com>
2022-05-29 20:17:36 +0200

    converter date from datepicker

--- src/app/modules/helpers/dateHelpers.spec.ts
+++ src/app/modules/helpers/dateHelpers.spec.ts
@@ -5,4 +5,8 @@ describe("conversion format should work",()=>{
   it("from Italian to american",()=>{
     expect(test.fromItalian2AmericanFormat("16/03/1977")).toBe("03/16/1977")
   })
+  it("convert from datePicker",()=>{
+    const test = new DateHelpers()
+    expect(test.fromDatePickerFormat2ItalianFormat("2022-05-24")).toBe("24/05/2022")
+  })
 })
