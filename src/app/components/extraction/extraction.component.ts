f51c96cd639947554cab5b6fea3fe6c48b3461d6
arpho <damicogiuseppe77@gmail.com>
2022-06-03 16:29:51 +0200

    refactoring for observable
    to interact with the user we should use observable

--- src/app/components/extraction/extraction.component.ts
+++ src/app/components/extraction/extraction.component.ts
@@ -1,4 +1,5 @@
 import { Component, Input, OnChanges, OnInit, SimpleChanges } from '@angular/core';
+import { Observable } from 'rxjs';
 import { Extraction } from 'src/app/models/extractionModel';
 
 @Component({
@@ -7,13 +8,21 @@ import { Extraction } from 'src/app/models/extractionModel';
   styleUrls: ['./extraction.component.scss'],
 })
 export class ExtractionComponent implements OnInit, OnChanges {
-  @Input() extraction:Extraction
+  @Input() extraction:Observable<Extraction> = new Observable()
+  selectedExtraction: Extraction
 
   constructor() { }
   ngOnChanges(changes: SimpleChanges): void {
     console.log("change",changes)
   }
 
-  ngOnInit() {}
+  ngOnInit() {
+
+    console.log("extraction",this.extraction)
+    this.extraction.subscribe((e)=>{
+      console.log("subscription",e)
+      this.selectedExtraction = e
+    })
+  }
 
 }
