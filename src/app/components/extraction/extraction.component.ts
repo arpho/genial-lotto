64c3bd9edda1c90c9037c382085cc6f242dff202
arpho <damicogiuseppe77@gmail.com>
2022-06-03 15:46:38 +0200

    Iput extraction
    implemented input field

--- src/app/components/extraction/extraction.component.ts
+++ src/app/components/extraction/extraction.component.ts
@@ -1,13 +1,18 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnChanges, OnInit, SimpleChanges } from '@angular/core';
+import { Extraction } from 'src/app/models/extractionModel';
 
 @Component({
   selector: 'app-extraction',
   templateUrl: './extraction.component.html',
   styleUrls: ['./extraction.component.scss'],
 })
-export class ExtractionComponent implements OnInit {
+export class ExtractionComponent implements OnInit, OnChanges {
+  @Input() extraction:Extraction
 
   constructor() { }
+  ngOnChanges(changes: SimpleChanges): void {
+    console.log("change",changes)
+  }
 
   ngOnInit() {}
 
