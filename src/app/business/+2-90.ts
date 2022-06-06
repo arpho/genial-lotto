9acd3a0a3146d713cfc0dbeb09a298ddbcbedfd8
Giuseppe D'Amico <damicogiuseppe77@gmail.com>
2022-06-06 11:56:31 +0200

    transformations
    implemented interface for transformation

---
+++ src/app/business/+2-90.ts
@@ -0,0 +1,6 @@
+import { TransformationInterface } from "../models/trasformationInterface";
+
+export class piu2meno90 implements TransformationInterface{
+    transform: (estratto: number) => number;
+    
+}
