f15e1f309bb4a9da0a46b5a070d29aaee1fc616f
Giuseppe D'Amico <damicogiuseppe77@gmail.com>
2022-06-07 08:41:40 +0200

    Figura
    figura implemented and tested

---
+++ src/app/business/figura.ts
@@ -0,0 +1,9 @@
+import { TransformationInterface } from "../models/trasformationInterface";
+
+export class Figura implements TransformationInterface{
+    transform: (estratto: number) => number=(estratto:number)=>{
+        const quoziente =Math.floor(estratto/10)
+        const resto= estratto%10
+        return estratto<=9?estratto:this.transform(quoziente+resto)
+    }
+}
