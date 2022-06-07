b8620a23dacbe6bd8a822beb8df5f9fe6f3b7e4a
Giuseppe D'Amico <damicogiuseppe77@gmail.com>
2022-06-07 08:23:33 +0200

    renamed
    function renamed in piu2meno90

--- src/app/business/trasformazioni.spec.ts
+++
@@ -1,9 +0,0 @@
-import { Trasformazioni } from "./trasformazioni"
-
-describe("trasformazioni funzionana",()=>{
-  it("+2-90",()=>{
-    const transf = new Trasformazioni()
-    expect(transf.piu2meno90(8)).toBe(80)
-    expect(transf.piu2meno90(10)).toBe(78)
-  })
-})
