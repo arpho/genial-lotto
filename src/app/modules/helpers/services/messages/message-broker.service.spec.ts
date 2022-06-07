ffd3d6e8257bc823299b1663befbaadb1811cd81
Giuseppe D'Amico <damicogiuseppe77@gmail.com>
2022-06-07 11:39:29 +0200

    message-broker service
    #TODO complete tests

---
+++ src/app/modules/helpers/services/messages/message-broker.service.spec.ts
@@ -0,0 +1,36 @@
+import { TestBed } from '@angular/core/testing';
+
+import { MessageBrokerService } from './message-broker.service';
+
+describe('MessageBrokerService', () => {
+  let service: MessageBrokerService;
+
+  beforeEach(() => {
+    TestBed.configureTestingModule({});
+    service = TestBed.inject(MessageBrokerService);
+  });
+
+  it('should be created', () => {
+    expect(service).toBeTruthy();
+  });
+  it("should add broker",()=>{
+    service.addBroker("test")
+    expect(service.brokers['test']).toBeDefined()
+  })
+  it("should not subscribe",()=>{
+    expect(service.subscribeTo("test",()=>{})).toBeUndefined()
+  })
+  it("should subscribe",()=>{
+    service.addBroker("test")
+    expect(service.brokersList().length).toBe(1)
+    expect(service.subscribeTo('test',()=>{})).toBeDefined()
+  })
+  it("should not publish",()=>{
+    expect(service.publish("test",2)).toBeFalse()
+  })
+  it("should publish",()=>{
+    service.addBroker("test")
+    expect(service.publish("test",2)).toBeTrue()
+    
+  })
+});
