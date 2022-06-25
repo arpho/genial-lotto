206e9916f62933f9da37d8432e4e14aaaa27f81d
arpho <damicogiuseppe77@gmail.com>
2022-06-25 15:52:15 +0200

    CusstomersService
    injected customersService in the  customers page

--- src/app/pages/customers/list/customers/customers.page.ts
+++ src/app/pages/customers/list/customers/customers.page.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { CustomersService } from 'src/app/services/customers/customers-service.service';
 
 @Component({
   selector: 'app-customers',
@@ -7,7 +8,7 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CustomersPage implements OnInit {
 
-  constructor() { }
+  constructor(service:CustomersService) { }
 
   ngOnInit() {
   }
