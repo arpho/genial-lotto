9345ed699aa0a52501db5a5aabe23e66fbc54649
arpho <damicogiuseppe77@gmail.com>
2022-06-27 11:27:51 +0200

    refactoring
    indented code

--- src/app/pages/modals/apply-function2-weels/apply-function2-weels.page.ts
+++ src/app/pages/modals/apply-function2-weels/apply-function2-weels.page.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { NavParams } from '@ionic/angular';
+import { ModalController, NavParams } from '@ionic/angular';
 import { Extraction } from 'src/app/models/extractionModel';
 import { TransformationInterface } from 'src/app/models/trasformationInterface';
 
@@ -9,25 +9,32 @@ import { TransformationInterface } from 'src/app/models/trasformationInterface';
   styleUrls: ['./apply-function2-weels.page.scss'],
 })
 export class ApplyFunction2WeelsPage implements OnInit {
-weel1:String
-date:string
-weel2:String
-WeelOne:Extraction
-WeelTwo:Extraction
-function:TransformationInterface
-extractions:Extraction[]
-  constructor(public navParams:NavParams) { }
+  weel1: String
+  date: string
+  weel2: String
+  WeelOne: Extraction
+  WeelTwo: Extraction
+  function: TransformationInterface
+  extractions: Extraction[]
+  constructor(
+    public navParams: NavParams,
+    public modalCtrl: ModalController
+  ) { }
+
+  dismisss(){
+    this.modalCtrl.dismiss()
+  }
 
   ngOnInit() {
-    this.weel1= this.navParams.get("weel1")
-    this.weel2= this.navParams.get("weel2")
-    this.date= this.navParams.get("date")
-    this.function= this.navParams.get("function")
-    this.extractions= this.navParams.get("extractions")
-    this.WeelOne =   this.extractions.filter((e: Extraction) => {
+    this.weel1 = this.navParams.get("weel1")
+    this.weel2 = this.navParams.get("weel2")
+    this.date = this.navParams.get("date")
+    this.function = this.navParams.get("function")
+    this.extractions = this.navParams.get("extractions")
+    this.WeelOne = this.extractions.filter((e: Extraction) => {
       return e.weel == this.weel1 && e.italianDate == this.date
     })[0]
-    this.WeelTwo =   this.extractions.filter((e: Extraction) => {
+    this.WeelTwo = this.extractions.filter((e: Extraction) => {
       return e.weel == this.weel2 && e.italianDate == this.date
     })[0]
 
