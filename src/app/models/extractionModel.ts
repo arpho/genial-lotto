71faad6ebbcdd8e3655a7010b054367c3013d6ec
arpho <damicogiuseppe77@gmail.com>
2022-06-28 17:32:21 +0200

    itemModelInterface
    Extraction implements itemModel Interface

--- src/app/models/extractionModel.ts
+++ src/app/models/extractionModel.ts
@@ -2,10 +2,14 @@ import { stringify } from "querystring";
 import { ExtractionValidator } from "../business/extractionValidator";
 import { DateHelpers } from "../modules/helpers/dateHelper";
 import { Serializers } from "../modules/helpers/serializers"
+import { Genere, ItemModelInterface } from "../modules/item/models/itemModelInterface";
+import { ItemServiceInterface } from "../modules/item/models/ItemServiceInterface";
+import { QuickAction } from "../modules/item/models/QuickAction";
+import { Value } from "../modules/item/models/value";
 import { TransformationInterface } from "./trasformationInterface";
 
 
-export class Extraction {
+export class Extraction implements ItemModelInterface {
 
   _date: string
   _dateInmsec: number
@@ -109,10 +113,65 @@ get extraction(){
   constructor(v?: {}){
     this.load(v)
   }
+  note?: string;
+  key: string;
+  quickActions?: QuickAction[];
+  archived?: boolean;
+  service?: ItemServiceInterface;
+  getTitle(): Value {
+    throw new Error("Method not implemented.");
+  }
+  getCountingText(): { singular: string; plural: string; } {
+    return {singular:"estrazioone",plural:"estrazioni"}
+  }
+  getNote(): Value {
+   return new Value({value:"",label:"nota"})
+  }
+  build?(item: {}) {
+   return this.load(item);
+  }
+  isArchived?(): boolean {
+    return false
+  }
+  archiveItem?(b: boolean) {
+    
+  }
+  isArchivable?(): boolean {
+  return false
+  }
+  getValue2(): Value {
+    return new Value({value:this.weel,label:"ruota di"})
+  }
+  getValue3(): Value {
+    return new Value({value:this.date,label:"estrazione del"})
+  }
+  getValue4(): Value {
+    return new Value({value:this.extraction.join('-'),label:"numeri estratti"})
+  }
+  setKey?(key: string): ItemModelInterface {
+    this.key = key
+    return this
+  }
+  getEditPopup(item?: ItemModelInterface, service?: ItemServiceInterface) {
+    throw new Error("Method not implemented.");
+  }
+  initialize(item: {}): ItemModelInterface {
+    throw new Error("Method not implemented.");
+  }
+  getAggregate(): Value {
+    throw new Error("Method not implemented.");
+  }
+  hasQuickActions?(): boolean {
+    return false
+  }
+  getElement(): { element: string; genere: Genere; } {
+    return {element:"estrazione",genere:'a'}
+  }
 
   serialize() {
     const serializers = new Serializers()
     return {
+      "date":serializers.serialize2String(this.date),
       "dateInmsec": serializers.serialize2PositiveNumber(this._dateInmsec, -1),
       "weel": serializers.serialize2String(this.weel),
       "extraction": serializers.serialize2Array(this._extraction)
