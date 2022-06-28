76bb4623433a8de7d9051d0d660909bc76846349
arpho <damicogiuseppe77@gmail.com>
2022-06-28 11:09:21 +0200

    dismiss addExtraction
    implemented button to dismis the modal

--- src/app/pages/modals/add-extraction/add-extraction.page.ts
+++ src/app/pages/modals/add-extraction/add-extraction.page.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { ModalController } from '@ionic/angular';
 
 @Component({
   selector: 'app-add-extraction',
@@ -7,9 +8,15 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AddExtractionPage implements OnInit {
 
-  constructor() { }
+  constructor(public modalCtrl:ModalController) { }
+
+
 
   ngOnInit() {
+  }
+
+  dismiss(v?:any){
+    this.modalCtrl.dismiss(v)
   }
 
 }
