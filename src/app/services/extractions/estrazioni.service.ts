f08bbab4d23b890f5cdc2465322768d48d260957
arpho <damicogiuseppe77@gmail.com>
2022-06-28 17:40:42 +0200

    ItemService
    implemented interface ItemServiceInterface

--- src/app/services/extractions/estrazioni.service.ts
+++ src/app/services/extractions/estrazioni.service.ts
@@ -4,18 +4,20 @@ import { Extraction } from 'src/app/models/extractionModel';
 import { collection, doc, setDoc, getDocs } from "firebase/firestore"; 
 import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
-import { getDatabase,onValue,push,ref} from 'firebase/database';
+import { Database, DatabaseReference, getDatabase,onValue,push,ref, set} from 'firebase/database';
+import { ItemServiceInterface } from 'src/app/modules/item/models/ItemServiceInterface';
+import { ItemModelInterface } from 'src/app/modules/item/models/itemModelInterface';
 
 @Injectable({
   providedIn: 'root'
 })
-export class ExtractionService {
+export class ExtractionService implements ItemServiceInterface {
   collection= "extractions"
   extractions_list:Extraction[]
   _extractions: BehaviorSubject<Array<Extraction>> = new BehaviorSubject([])
   readonly extractions:Observable<Array<Extraction>> = this._extractions.asObservable()
 
-  async loadAndPublish(){
+  async loadDataAndPublish(){
    const db = getDatabase()
    const extractionReference = ref(db,this.collection)
    
@@ -36,6 +38,32 @@ export class ExtractionService {
     return  push(extractionReference,extraction.serialize())
   }
   constructor() {
-    this.loadAndPublish()
+    this.loadDataAndPublish()
    }
+  categoriesService?: ItemServiceInterface;
+  suppliersService?: ItemServiceInterface;
+  paymentsService?: ItemServiceInterface;
+  reference: string;
+  _items: BehaviorSubject<ItemModelInterface[]>;
+  items_list: ItemModelInterface[];
+  db: Database;
+  itemsListRef: DatabaseReference;
+  items: Observable<ItemModelInterface[]>;
+  getItem(key: string, next: (item?: any) => void): void {
+    const customerRef = ref(this.db, `${this.reference}/${key}`)
+    onValue(customerRef, (item => {
+      next(item.val())
+    }))
+  }
+  updateItem(item: ItemModelInterface) {
+    const reference = ref(this.db, `${this.reference}/${item.key}`)
+   return  set(reference, item.serialize())
+  }
+  deleteItem(key: string) {
+    const reference = ref(this.db, `${this.reference}/${key}`)
+    return set(reference, null)
+  }
+  getEmptyItem(): ItemModelInterface {
+    return new Extraction()
+  }
 }
