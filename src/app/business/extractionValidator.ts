a4e05995acd6661caeb88a0888d92dbb88341adf
arpho <damicogiuseppe77@gmail.com>
2022-06-28 11:55:39 +0200

    comment

--- src/app/business/extractionValidator.ts
+++ src/app/business/extractionValidator.ts
@@ -1,5 +1,7 @@
 export class ExtractionValidator{
-isValid(extraction:number[]){
+
+isValid(extraction:number[]){/*
+* valid only when is an array of 5 positive integer number <90 */
  return extraction.map(e=>{
     return typeof e =='number' && e<=90 && Number.isInteger(e) && e>0?1:0
   }).reduce((pv,cv)=>{
