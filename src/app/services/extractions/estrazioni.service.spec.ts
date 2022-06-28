8f22f1c21fcfdd6d91d91e40fd33be116da19112
arpho <damicogiuseppe77@gmail.com>
2022-06-28 16:45:56 +0200

    renaming
    estrazioniService renamed in ExtractionSevice

--- src/app/services/extractions/estrazioni.service.spec.ts
+++ src/app/services/extractions/estrazioni.service.spec.ts
@@ -1,13 +1,13 @@
 import { TestBed } from '@angular/core/testing';
 
-import { EstrazioniService } from './estrazioni.service';
+import { ExtractionService } from './estrazioni.service';
 
 describe('EstrazioniService', () => {
-  let service: EstrazioniService;
+  let service: ExtractionService;
 
   beforeEach(() => {
     TestBed.configureTestingModule({});
-    service = TestBed.inject(EstrazioniService);
+    service = TestBed.inject(ExtractionService);
   });
 
   it('should be created', () => {
