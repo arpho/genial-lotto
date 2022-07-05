768f89cacc3ecdace9278cc5b318466b64f3bcf6
arpho <damicogiuseppe77@gmail.com>
2022-07-05 14:13:26 +0200

    console.log
    removed debug code

--- src/app/business/intervalCalculator.ts
+++ src/app/business/intervalCalculator.ts
@@ -13,16 +13,12 @@ retrieveInterval(weel:string,numero:number,date:string):Interval[]
 const intervals = []
 const extractions4Weel = this.extractions.filter(e=>{ 
   return e.weel==weel})
-  console.log("#* extraction 4 weel",extractions4Weel)
 const startIndex = extractions4Weel.map(e=>new DateModel( new Date(e.dateInmmsec)).ItalianFormatDate()).indexOf(date) // the position where to start retrieving interval from
 var count=0
-console.log("start index #*",startIndex)
 for( let i = startIndex+1; i< extractions4Weel.length;i++){
   count+=1
-  console.log("iterating#*",extractions4Weel[i],extractions4Weel[i].extraction.includes(numero))
   if(extractions4Weel[i].extraction.includes(numero)){
     const interval = new Interval(count,extractions4Weel[i].italianDate)
-    console.log("pushing #*",interval)
     intervals.push(interval)
     count =0 // reset the counter
   }
