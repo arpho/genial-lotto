5131b527650d0ddd5e5c39d935da0816ef2a6347
arpho <damicogiuseppe77@gmail.com>
2022-07-06 20:23:27 +0200

    folder page modal
     showing the modal creates strange problems,the graph on labels area does not show up

--- src/app/folder/folder.page.ts
+++ src/app/folder/folder.page.ts
@@ -141,7 +141,7 @@ export class FolderPage implements OnInit {
       component: ApplyFunction2WeelsPage, componentProps: props
     })
     this.messages.publish("ambate",props)
-    await modal.present()
+    //await modal.present()
   }
 
   transformation: TransformationInterface
