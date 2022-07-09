7c23e899d5552dd23988c84f27086214a93cfdb9
arpho <damicogiuseppe77@gmail.com>
2022-07-10 00:22:48 +0200

    ambate highlighted
    implemented code to highlight number

--- src/app/pages/extractions/browsing/browsing.page.ts
+++ src/app/pages/extractions/browsing/browsing.page.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { ModalController, NavParams } from '@ionic/angular';
+import { Ambate } from 'src/app/models/ambate';
 import { Extraction } from 'src/app/models/extractionModel';
 
 @Component({
@@ -14,8 +15,11 @@ ambata11:number
 ambata12:number
 ambata21:number
 ambata22:number
+ambate:Ambate
 weel1:string
 weel2:string
+color1 = "red"
+color2 = "orange"
 date:string
   constructor(
     public modalCtrl:ModalController,
@@ -32,6 +36,13 @@ date:string
     this.ambata21 = this.navParams.get("ambata21")
     this.ambata22 = this.navParams.get("ambata22")
     this.extractionsItems=this.navParams.get("extractions")
+    this.ambate= new Ambate({
+      weel1:this.weel1,
+      weel2:this.weel2,
+      ambata11:this.ambata11,
+      ambata21:this.ambata21
+    })
+    console.log("ambata",this.ambate)
   }
   dismiss(value?:{}){
     this.modalCtrl.dismiss(value)
