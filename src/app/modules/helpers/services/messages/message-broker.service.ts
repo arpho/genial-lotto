6adf6d9bb20bad587c1cbf639245adabd5fa1f69
arpho <damicogiuseppe77@gmail.com>
2022-07-10 22:42:00 +0200

    strange behaviour
    withouth the console.log the subscription to claims receives null

--- src/app/modules/helpers/services/messages/message-broker.service.ts
+++ src/app/modules/helpers/services/messages/message-broker.service.ts
@@ -27,6 +27,7 @@ export class MessageBrokerService {
     return Object.keys(this.brokers)
   }
   publish(key:string,msg:{}){
+    console.log("publishing",key,msg)
     var out = true
     if(this._brokers[key]){
       this._brokers[key].next(msg)
