e939743493ecbf658d2bc0ba467b9a081e205b59
arpho <damicogiuseppe77@gmail.com>
2022-07-11 12:50:34 +0200

    indentetion and filtering
    we show only the dates successive to the choosed one

--- src/app/pages/extractions/browsing/browsing.page.ts
+++ src/app/pages/extractions/browsing/browsing.page.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { ModalController, NavParams } from '@ionic/angular';
 import { Ambate } from 'src/app/models/ambate';
 import { Extraction } from 'src/app/models/extractionModel';
+import { DateHelpers } from 'src/app/modules/helpers/dateHelper';
 
 @Component({
   selector: 'app-browsing',
@@ -9,42 +10,48 @@ import { Extraction } from 'src/app/models/extractionModel';
   styleUrls: ['./browsing.page.scss'],
 })
 export class BrowsingPage implements OnInit {
-extractionsItems:Extraction[]
-dateEstrazioni:string[]
-ambata11:number
-ambata12:number
-ambata21:number
-ambata22:number
-ambate:Ambate
-weel1:string
-weel2:string
-color1 = "red"
-color2 = "orange"
-date:string
+  extractionsItems: Extraction[]
+  dateEstrazioni: string[]
+  ambata11: number
+  ambata12: number
+  ambata21: number
+  ambata22: number
+  ambate: Ambate
+  weel1: string
+  weel2: string
+  color1 = "red"
+  color2 = "orange"
+  date: string
   constructor(
-    public modalCtrl:ModalController,
-    public navParams:NavParams) { }
+    public modalCtrl: ModalController,
+    public navParams: NavParams) { }
+
+  fromItalianDate2UsDate(d: string) {
+    const transformer = new DateHelpers()
+    return transformer.fromItalian2AmericanFormat(d)
+  }
 
   ngOnInit() {
-    this.extractionsItems= this.navParams.get("extractions")
-    this.date= this.navParams.get("date")
-    this.dateEstrazioni= this.dateEstrazioni
-    this.weel1= this.navParams.get("weel1")
+    this.extractionsItems = this.navParams.get("extractions")
+    this.date = this.navParams.get("date")
+    this.dateEstrazioni = this.dateEstrazioni.filter(d =>
+      new Date(this.fromItalianDate2UsDate(d)).getTime() <= new Date(this.fromItalianDate2UsDate(this.date)).getTime())
+    this.weel1 = this.navParams.get("weel1")
     this.weel2 = this.navParams.get("weel2")
     this.ambata11 = this.navParams.get("ambata11")
     this.ambata12 = this.navParams.get("ambata12")
     this.ambata21 = this.navParams.get("ambata21")
     this.ambata22 = this.navParams.get("ambata22")
-    this.extractionsItems=this.navParams.get("extractions")
-    this.ambate= new Ambate({
-      weel1:this.weel1,
-      weel2:this.weel2,
-      ambata11:this.ambata11,
-      ambata21:this.ambata21
+    this.extractionsItems = this.navParams.get("extractions")
+    this.ambate = new Ambate({
+      weel1: this.weel1,
+      weel2: this.weel2,
+      ambata11: this.ambata11,
+      ambata21: this.ambata21
     })
-    console.log("ambata",this.ambate)
+    console.log("ambata", this.ambate)
   }
-  dismiss(value?:{}){
+  dismiss(value?: {}) {
     this.modalCtrl.dismiss(value)
   }
 
