17068f0deaf787e3e8075d1e83b482df46983a9e
arpho <damicogiuseppe77@gmail.com>
2022-07-12 17:20:01 +0200

    refactored repetitionFinder
    clearer code

--- src/app/business/repetionFinder.ts
+++ src/app/business/repetionFinder.ts
@@ -9,11 +9,12 @@ export class RepetitionsFinder{
 
   findRepetitions(otherWeel?:number[]){
     const vertibile= new Vertibile()
-    return this.extraction.map(item=>{
+    const output = this.extraction.map(item=>{
       return this.extraction.includes(vertibile.transform(item))||otherWeel?.includes(item)||
       otherWeel?.includes(vertibile.transform(item))
       ?1:0
     })
+    return  output
   }
 
 }
