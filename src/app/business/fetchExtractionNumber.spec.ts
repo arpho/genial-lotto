13f099411a5375a5cd976531506410e59f0f0d7d
arpho <damicogiuseppe77@gmail.com>
2022-07-12 15:33:01 +0200

    Unit Test: Custom Pipe error The pipe could not be found
    declared the erequested pipe

---
+++ src/app/business/fetchExtractionNumber.spec.ts
@@ -0,0 +1,13 @@
+import { FetchExtractionNumbers } from "./fetchExtractionNumbers"
+
+describe("we fetch extraction numbers",()=>{
+  it("fetch from data with single space",()=>{
+    const data= " 12 3 4 5 6"
+    expect(FetchExtractionNumbers.fetchNumbersFromInputString(data).length).toBe(5)
+    expect(FetchExtractionNumbers.fetchNumbersFromInputString(data)[0]).toBe(12)
+    expect(FetchExtractionNumbers.fetchNumbersFromInputString(data)[1]).toBe(3)
+    expect(FetchExtractionNumbers.fetchNumbersFromInputString(data)[2]).toBe(4)
+    expect(FetchExtractionNumbers.fetchNumbersFromInputString(data)[3]).toBe(5)
+    expect(FetchExtractionNumbers.fetchNumbersFromInputString(data)[4]).toBe(6)
+  })
+})
