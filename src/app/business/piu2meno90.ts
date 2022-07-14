c1718d23490b85b6d75ec2561ce33a5b16a45edd
arpho <damicogiuseppe77@gmail.com>
2022-07-14 11:45:55 +0200

    renaming
    renamed +2 -90 in Genial

--- src/app/business/piu2meno90.ts
+++ src/app/business/piu2meno90.ts
@@ -2,7 +2,7 @@ import { TransformationInterface } from "../models/trasformationInterface";
 
 export class Piu2meno90 implements TransformationInterface{
   
-  title = "+2-90"
+  title = "Genial"
     transform (estratto: number) {
         return Math.abs( 90-(estratto +2))
 
