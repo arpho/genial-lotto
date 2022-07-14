b770203ca47f006c4f3971c539f30c5fad0070ed
arpho <damicogiuseppe77@gmail.com>
2022-07-14 11:46:23 +0200

    renaming

--- src/app/business/piu2meno90.ts
+++ src/app/business/piu2meno90.ts
@@ -2,7 +2,7 @@ import { TransformationInterface } from "../models/trasformationInterface";
 
 export class Piu2meno90 implements TransformationInterface{
   
-  title = "Genial"
+  title = "Algoritmo Genial"
     transform (estratto: number) {
         return Math.abs( 90-(estratto +2))
 
