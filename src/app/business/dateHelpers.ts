71a78397caf2658b5d9c7c22ea25e461b170b88d
arpho <damicogiuseppe77@gmail.com>
2022-07-15 15:01:38 +0200

    implented sorterAscendingDate
     implemented new method and tested both

--- src/app/business/dateHelpers.ts
+++ src/app/business/dateHelpers.ts
@@ -1,4 +1,6 @@
 import { Extraction } from "../models/extractionModel";
 
-export class DateHelpers{ static sorterDescendingDate  = (b:Extraction,a:Extraction)=>a.dateInmmsec-b.dateInmmsec
+export class DateHelpers{ 
+  static sorterDescendingDate  = (b:Extraction,a:Extraction)=>a.dateInmmsec-b.dateInmmsec
+  static sorterAscendingDate = (a:Extraction,b:Extraction)=>a.dateInmmsec-b.dateInmmsec
 }
