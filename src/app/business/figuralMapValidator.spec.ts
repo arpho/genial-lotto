60979ef7f385ab184839807e2b8c216e07425992
arpho <damicogiuseppe77@gmail.com>
2022-07-16 09:37:13 +0200

    renaming figuralMapValidator
    adaptede the tests

--- src/app/business/figuralMapValidator.spec.ts
+++ src/app/business/figuralMapValidator.spec.ts
@@ -1,28 +1,28 @@
-import { FiguralMapValidator } from "./figuralMapValidator"
+import { FiguralMap } from "./figuralMap"
 
 describe("tfiguralMapValidators work",()=>{
   it("should instantiated",()=>{
-    const test  = new FiguralMapValidator()
+    const test  = new FiguralMap()
     expect(test).toBeDefined()
   })
   it("should split numbers",()=>{
-    expect(FiguralMapValidator.splitNumbers(11).length).toBe(2)
-    expect(FiguralMapValidator.splitNumbers(11)[0]).toBe(1)
-    expect(FiguralMapValidator.splitNumbers(11)[1]).toBe(1)
-    expect(FiguralMapValidator.splitNumbers(71)[0]).toBe(1)
-    expect(FiguralMapValidator.splitNumbers(71)[1]).toBe(7)
+    expect(FiguralMap.splitNumbers(11).length).toBe(2)
+    expect(FiguralMap.splitNumbers(11)[0]).toBe(1)
+    expect(FiguralMap.splitNumbers(11)[1]).toBe(1)
+    expect(FiguralMap.splitNumbers(71)[0]).toBe(1)
+    expect(FiguralMap.splitNumbers(71)[1]).toBe(7)
   })
 })
 it("isValid works",()=>{
-  expect(FiguralMapValidator.isValid("11.25.36")).toBe(true)
-  expect(FiguralMapValidator.isValid("11.25")).toBe(false)
+  expect(FiguralMap.isValid("11.25.36")).toBe(true)
+  expect(FiguralMap.isValid("11.25")).toBe(false)
 })
 it("should fetch the map",()=>{
-  expect(FiguralMapValidator.fetchMap("12.32.56").length).toBe(6)
-  expect(FiguralMapValidator.fetchMap("12.34.56")[0]).toBe(1)
-  expect(FiguralMapValidator.fetchMap("12.34.56")[1]).toBe(2)
-  expect(FiguralMapValidator.fetchMap("12.34.56")[2]).toBe(3)
-  expect(FiguralMapValidator.fetchMap("12.34.56")[3]).toBe(4)
-  expect(FiguralMapValidator.fetchMap("12.34.56")[4]).toBe(5)
-  expect(FiguralMapValidator.fetchMap("12.34.56")[5]).toBe(6)
+  expect(FiguralMap.fetchMap("12.32.56").length).toBe(6)
+  expect(FiguralMap.fetchMap("12.34.56")[0]).toBe(1)
+  expect(FiguralMap.fetchMap("12.34.56")[1]).toBe(2)
+  expect(FiguralMap.fetchMap("12.34.56")[2]).toBe(3)
+  expect(FiguralMap.fetchMap("12.34.56")[3]).toBe(4)
+  expect(FiguralMap.fetchMap("12.34.56")[4]).toBe(5)
+  expect(FiguralMap.fetchMap("12.34.56")[5]).toBe(6)
 })
