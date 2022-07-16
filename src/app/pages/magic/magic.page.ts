786d8bf28ceac3cc7bf46b2321bb3afb9b965f4d
arpho <damicogiuseppe77@gmail.com>
2022-07-16 09:42:43 +0200

    dismiss magic page
    implementation has been completed, but there is a strange error

--- src/app/pages/magic/magic.page.ts
+++ src/app/pages/magic/magic.page.ts
@@ -1,15 +1,27 @@
 import { Component, OnInit } from '@angular/core';
+import { ModalController } from '@ionic/angular';
 
 @Component({
-  selector: 'app-magic',
+  selector: 'app-magic-page',
   templateUrl: './magic.page.html',
   styleUrls: ['./magic.page.scss'],
 })
 export class MagicPage implements OnInit {
 
-  constructor() { }
+  constructor(public modalCtrl:ModalController) { }
+
+
 
   ngOnInit() {
+  }
+
+  ciao(){
+    console.log("ciao")
+    this.dismiss()
+  }
+
+ public  dismiss(value?:{}){
+    this.modalCtrl.dismiss(value)
   }
 
 }
