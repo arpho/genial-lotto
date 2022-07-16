c353841dccc3c8f829b12a9637857b44da253bf4
arpho <damicogiuseppe77@gmail.com>
2022-07-17 00:40:42 +0200

    refactored intervals-d3 for changes
    implemented code to make the graph when values changes

--- src/app/components/intervals-d3/intervals-d3.component.ts
+++ src/app/components/intervals-d3/intervals-d3.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input, OnDestroy, OnInit } from '@angular/core';
+import { Component, Input, OnChanges, OnDestroy, OnInit, SimpleChanges } from '@angular/core';
 import { DateHelpers } from 'src/app/business/dateHelpers';
 import { IntervalCalculator } from 'src/app/business/intervalCalculator';
 import { makeData4D3 } from 'src/app/business/makeData4D3';
@@ -10,20 +10,36 @@ import { ExtractionService } from 'src/app/services/extractions/estrazioni.servi
   templateUrl: './intervals-d3.component.html',
   styleUrls: ['./intervals-d3.component.scss'],
 })
-export class IntervalsD3Component implements OnInit,OnDestroy {
+export class IntervalsD3Component implements OnInit,OnDestroy,OnChanges {
 @Input() weel:string
 @Input() extractedNumber:number
 subscription
+subscription2
 barData:Interval[]
   constructor(private service:ExtractionService) { }
+  ngOnChanges(changes: SimpleChanges,): void {
+    this.extractedNumber = Number(this.extractedNumber) // the number arrives as string
+   this.service.items.subscribe(extractions=>{
+      const intervals= new IntervalCalculator(extractions
+        .sort(DateHelpers
+          .sorterDescendingDate))
+          .retrieveInterval(this.weel,this.extractedNumber)
+          const makeData = new makeData4D3()
+          this.barData = makeData.transform( intervals).slice(0,20)
+
+
+    })
+  }
   ngOnDestroy(): void {
     if(this.subscription){
       this.subscription.unsubscribe()
     }
+    if(this.subscription2){
+      this.subscription2.unsubscribe()
+    }
   }
 
   ngOnInit() {
-    console.log("weel",this.weel,this.extractedNumber)
     this.subscription = this.service.items.subscribe(extractions=>{
       const intervals= new IntervalCalculator(extractions
         .sort(DateHelpers
@@ -31,7 +47,6 @@ barData:Interval[]
           .retrieveInterval(this.weel,this.extractedNumber)
           const makeData = new makeData4D3()
           this.barData = makeData.transform( intervals).slice(0,20)
-          console.log("barData",this.barData)
 
 
     })
