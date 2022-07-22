143250c39f4bb39a151fc37312536006ebbe4f48
arpho <damicogiuseppe77@gmail.com>
2022-07-22 16:26:03 +0200

    removed old code

--- src/app/modules/helpers/dateHelper.ts
+++ src/app/modules/helpers/dateHelper.ts
@@ -1,6 +1,9 @@
 import { Extraction } from "src/app/models/extractionModel"
 
 export class DateHelpers{
+  static getDateListFrom(extractionsItems: Extraction[]): string[] {
+   return Array.from(new Set(extractionsItems.map(e=>e.italianDate)))
+  }
 
   converter(date){
     return `${date.split('/')[1]}/${date.split('/')[0]}/${date.split('/')[2]}`
