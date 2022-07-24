39e0ee3e057f7e3be0ba612b4405de92e456d1e0
arpho <damicogiuseppe77@gmail.com>
2022-07-24 11:40:55 +0200

    crud page ready to show data

--- src/app/pages/extractions/crud/crud.page.ts
+++ src/app/pages/extractions/crud/crud.page.ts
@@ -1,19 +1,38 @@
-import { Component, OnInit } from '@angular/core';
-import { ModalController } from '@ionic/angular';
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { Subscription } from 'rxjs';
+import { DateHelpers } from 'src/app/business/dateHelpers';
+import { OptionsMaker } from 'src/app/modules/dynamic-form/helpers/optionMaker';
+import { DropdownQuestion } from 'src/app/modules/dynamic-form/models/question-dropdown';
+import { ExtractionService } from 'src/app/services/extractions/estrazioni.service';
 
 @Component({
   selector: 'app-crud',
   templateUrl: './crud.page.html',
   styleUrls: ['./crud.page.scss'],
 })
-export class CrudPage implements OnInit {
+export class CrudPage implements OnInit,OnDestroy {
+  formFields =[]
+  dates :string[]=[]
+  subscriptions:Subscription=Subscription.EMPTY
 
-  constructor(private modalCtrl:ModalController) { }
+  constructor(public service:ExtractionService) { }
+  ngOnDestroy(): void {
+    this.subscriptions.unsubscribe()
+  }
 
   ngOnInit() {
+    this.subscriptions.add( this.service.items.subscribe(items=>{
+     const Items = items.sort(DateHelpers.sorterDescendingDate);
+     this.dates = Array.from(new Set(Items.map(ex => ex.italianDate)))
+     console.log("dates",this.dates)
+      this.formFields = [new DropdownQuestion({label:"scegli una data",
+    key:"date",
+    options:new OptionsMaker().makesOptionsFromArray(this.dates)})]
+    }))
   }
-  dismiss(value?:{}){
-    this.modalCtrl.dismiss(value)
+
+  setFilter(ev){
+    console.log("typing",ev)
   }
 
 }
