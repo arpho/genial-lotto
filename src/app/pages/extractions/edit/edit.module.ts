95ef5004ec5553fd7e8b24538aab1e93e5dea3a0
arpho <damicogiuseppe77@gmail.com>
2022-07-24 13:45:29 +0200

    edit page
    extraction edit page completed

--- src/app/pages/extractions/edit/edit.module.ts
+++ src/app/pages/extractions/edit/edit.module.ts
@@ -7,13 +7,15 @@ import { IonicModule } from '@ionic/angular';
 import { EditPageRoutingModule } from './edit-routing.module';
 
 import { EditPage } from './edit.page';
+import { DynamicFormModule } from 'src/app/modules/dynamic-form/dynamic-form.module';
 
 @NgModule({
   imports: [
     CommonModule,
     FormsModule,
     IonicModule,
-    EditPageRoutingModule
+    EditPageRoutingModule,
+    DynamicFormModule
   ],
   declarations: [EditPage]
 })
--- src/app/pages/extractions/edit/edit.page.ts
+++ src/app/pages/extractions/edit/edit.page.ts
@@ -1,4 +1,11 @@
 import { Component, OnInit } from '@angular/core';
+import { ModalController, NavParams } from '@ionic/angular';
+import { Extraction } from 'src/app/models/extractionModel';
+import { TextboxQuestion } from 'src/app/modules/dynamic-form/models/question-textbox';
+import { extractionValidator4Field } from 'src/app/business/ExtractioValidator4Field';
+import { FetchExtractionNumbers } from 'src/app/business/fetchExtractionNumbers';
+import { ExtractionService } from 'src/app/services/extractions/estrazioni.service';
+import { MyToastService } from 'src/app/modules/helpers/services/toaster/my-toast-service.service';
 
 @Component({
   selector: 'app-edit',
@@ -6,10 +13,44 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./edit.page.scss'],
 })
 export class EditPage implements OnInit {
-
-  constructor() { }
+extraction:Extraction
+formFields= []
+  constructor(private navParams:NavParams,
+      private modalCtrl:ModalController,
+      public service:ExtractionService,
+      private toaster: MyToastService
+    ) { }
 
   ngOnInit() {
+    this.extraction = this.navParams.get('item')
+
+    console.log("estrazione",this.extraction)
+    this.formFields= [new TextboxQuestion({
+      key:"weel",
+      label:this.extraction.weel,
+      value:this.extraction.extraction.join(' '),
+      validator:extractionValidator4Field
+    })]
+    console.log("form",this.formFields)
+  }
+
+  dismiss(value?:{}){
+    this.modalCtrl.dismiss(value)
+  }
+
+  filter(ev){
+    console.log("typing",ev)
+  }
+  submit(ev){
+    console.log("submitting",ev)
+    const updatedExtraction = new Extraction(this.extraction)
+    updatedExtraction.italianDate= this.extraction.italianDate
+    updatedExtraction.setKey(this.extraction.key)
+    updatedExtraction.extraction = FetchExtractionNumbers.fetchNumbersFromInputString(ev.weel)
+    console.log("submitting",updatedExtraction)
+    this.service.updateItem(updatedExtraction).then(()=>{
+      this.toaster.presentToast("estrazione modificata regolarmente")
+    })
   }
 
 }
