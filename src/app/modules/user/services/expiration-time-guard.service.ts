9badd4999db94022a366675e112e53f2bbbcae38
arpho <damicogiuseppe77@gmail.com>
2022-07-28 19:25:23 +0200

    expirationTimeGuard
     implemented and tested

--- src/app/modules/user/services/expiration-time-guard.service.ts
+++ src/app/modules/user/services/expiration-time-guard.service.ts
@@ -1,9 +1,40 @@
 import { Injectable } from '@angular/core';
+import { ActivatedRouteSnapshot, CanActivate, Router, RouterStateSnapshot, UrlTree } from '@angular/router';
+import { initializeApp } from 'firebase/app';
+import { getAuth, onAuthStateChanged, User } from 'firebase/auth';
+import { Observable } from 'rxjs';
+import { credentials } from 'src/app/configs/credentials';
 
 @Injectable({
   providedIn: 'root'
 })
-export class ExpirationTimeGuardService {
+export class ExpirationTimeGuard implements CanActivate {
 
-  constructor() { }
+  constructor(private router:Router) { }
+  isCurrent(expirationTime:string){
+    return Number(expirationTime)>= new Date().getTime()
+  }
+  canActivate(route: ActivatedRouteSnapshot,
+     state: RouterStateSnapshot):
+      boolean | UrlTree | Observable<boolean | UrlTree> | Promise<boolean | UrlTree> {
+
+        const app = initializeApp(credentials.firebase)
+        let out = false
+        const auth = getAuth(app)
+        onAuthStateChanged(auth,(async (user:User)=>{
+          auth.currentUser.getIdTokenResult(true).then(token=>{
+            if(this.isCurrent(token.expirationTime)){
+              out =  true
+            }
+            else{
+              const message =
+                `il tuo account è scaduto 
+                 per chiarimenti rivolgiti all'amministratore`;
+              this.router.navigate(["users/not-authorized", message]);
+            }
+            
+          })
+        }))
+        return out
+  }
 }
