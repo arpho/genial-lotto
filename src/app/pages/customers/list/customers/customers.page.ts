9e8e753709cfa39da00a9a1e06df47c69aab45f2
arpho <damicogiuseppe77@gmail.com>
2022-07-30 20:12:00 +0200

    filtering customers
    implemented logic to filter customers

--- src/app/pages/customers/list/customers/customers.page.ts
+++ src/app/pages/customers/list/customers/customers.page.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { Customer } from 'src/app/models/Customer';
+import { TextboxQuestion } from 'src/app/modules/dynamic-form/models/question-textbox';
+import { ItemModelInterface } from 'src/app/modules/item/models/itemModelInterface';
 import { CustomersService } from 'src/app/services/customers/customers-service.service';
 import { NewCustomerPage } from '../../create/new-customer/new-customer.page';
 import { UpdateCustomerPage } from '../../edit/update-customer/update-customer.page';
@@ -17,9 +19,34 @@ export class CustomersPage implements OnInit {
     return true
   }
 
+  filterFields = [
+    new TextboxQuestion({
+      key:"nome",
+      label:"filtra per nome",
+      filterFunction:(value:string,customer:Customer)=>customer.
+      firstName.trim().toUpperCase().includes(value.trim().toUpperCase())
+    }),
+    new TextboxQuestion({
+      key:"surname",
+      label:"filtra per cognome",
+      filterFunction:(value:string,customer:Customer)=>customer.
+      lastName.trim().toUpperCase().includes(value.trim().toUpperCase())
+    }),
+    new TextboxQuestion({
+      key:"email",
+      label:"filtra per email",
+      filterFunction:(value:string,customer:Customer)=> customer.
+      email.trim().toLowerCase().includes(value.trim().toLowerCase())
+    })
+  ]
+
   constructor(public service:CustomersService) { }
 
   ngOnInit() {
+  }
+
+  setFilter(ev: (item: ItemModelInterface) => boolean) {
+    this.filterFunction = ev
   }
 
 }
